b9a329dfd03c1a421c3a9eeabaf5efeb9782c0ff
Lorenzo Manacorda <lorenzo@mailbox.org>
2021-01-31 13:58:18 +0200

    move flake.nix to root

--- flake.nix
+++ flake.nix
@@ -12,13 +12,14 @@
         pname = "substrate-telemetry-backend";
         version = "0.2.1";
 
-        src = ./.;
+        src = ./backend;
 
         nativeBuildInputs = [ pkg-config ];
         buildInputs = [ openssl ];
 
         cargoSha256 = "sha256-GczrMtSYNT979Tzup/92CoTJmNT8Pvy8O7syAu8EQPE=";
 
+        # https://github.com/asymmetric/substrate-telemetry/runs/1773146154?check_suite_focus=true#step:5:493
         doCheck = false;
       };
   };
